ec1358ec44cf6227f29bd0117a02a612799f9416
Xuân Trường <dxt8104@gmail.com>
2024-11-28 23:18:20 +0700

    push lần 2 thiết kế giao diện login

--- POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Login_Controller.java
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Login_Controller.java
@@ -1,4 +1,11 @@
 package vn.truongdx.poscoffee_app;
 
+import javafx.event.ActionEvent;
+
 public class Login_Controller {
+  //khai báo biến
+  //hàm thực hiện chức năng
+  public void Login(ActionEvent event) {
+
+  }
 }

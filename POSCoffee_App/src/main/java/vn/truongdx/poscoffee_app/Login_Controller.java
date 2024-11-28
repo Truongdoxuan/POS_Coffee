dcd288a5276d9aef540c8a208cc68d3e2ea12237
Xuân Trường <dxt8104@gmail.com>
2024-11-28 23:35:38 +0700

    push lần 4: viết hàm thực hiện login chuyển đến trang order

--- POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Login_Controller.java
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Login_Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -22,7 +23,11 @@ public class Login_Controller {
       FXMLLoader fxmlLoader = new FXMLLoader(Login_Controller.class.getResource("order_page.fxml"));
       Parent root = fxmlLoader.load();
       Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      Scene scene = new Scene(root);
+      stage.setScene(scene);
+      stage.show();
     } catch (Exception e) {
+      //nếu lỗi in lỗi ra terminal
       e.printStackTrace();
     }
 

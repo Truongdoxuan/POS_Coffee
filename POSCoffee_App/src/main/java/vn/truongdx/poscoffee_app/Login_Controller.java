0e3f49a683b29f7054649e9ad1c4912bc3057af7
Xuân Trường <dxt8104@gmail.com>
2024-11-28 23:27:47 +0700

    push lần 3 : tạo file fxml order và controller

--- POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Login_Controller.java
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Login_Controller.java
@@ -1,11 +1,30 @@
 package vn.truongdx.poscoffee_app;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Login_Controller {
   //khai báo biến
+  @FXML
+  TextField txt_maCH, txt_matkhau;
+  @FXML
+  Button btn_login;
   //hàm thực hiện chức năng
   public void Login(ActionEvent event) {
+    //onclick button chuyển đến trang order
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader(Login_Controller.class.getResource("order_page.fxml"));
+      Parent root = fxmlLoader.load();
+      Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
   }
 }
---
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Order_Controller.java
@@ -0,0 +1,4 @@
+package vn.truongdx.poscoffee_app;
+
+public class Order_Controller {
+}

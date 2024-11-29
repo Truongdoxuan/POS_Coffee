aec4eb9e64c5866b72955add0382dd671add4b6c
Xuân Trường <dxt8104@gmail.com>
2024-11-29 11:45:06 +0700

    push lần 12: viết hàm thực hiện chức năng file order_controller

--- POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Order_Controller.java
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Order_Controller.java
@@ -1,4 +1,11 @@
 package vn.truongdx.poscoffee_app;
 
+import javafx.event.ActionEvent;
+
 public class Order_Controller {
+  //khai báo biến
+  //hàm thực hiện chương trình
+  public void delete_Item(ActionEvent event) {
+    
+  }
 }

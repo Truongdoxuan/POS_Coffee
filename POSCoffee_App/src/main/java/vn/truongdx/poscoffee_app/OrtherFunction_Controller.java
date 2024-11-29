0f7ffd00fa6cecaaba463983606f9e05f2ab71f1
Xuân Trường <dxt8104@gmail.com>
2024-11-29 14:10:53 +0700

    push lần 17

--- POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/OrtherFunction_Controller.java
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/OrtherFunction_Controller.java
@@ -1,4 +1,20 @@
 package vn.truongdx.poscoffee_app;
 
+import javafx.event.ActionEvent;
+
 public class OrtherFunction_Controller {
+  //khai báo biến
+  //hàm thực hiện chức năng
+  public void end_Shifts(ActionEvent event) {
+
+  }
+  public void end_Day(ActionEvent event) {
+
+  }
+  public void Logout(ActionEvent event) {
+
+  }
+  public void close_Modal(ActionEvent event) {
+
+  }
 }

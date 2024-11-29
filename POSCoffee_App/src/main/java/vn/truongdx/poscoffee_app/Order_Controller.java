fdaf3a43d9024f2ab8a45e55d45ebd43142d0da4
Xuân Trường <dxt8104@gmail.com>
2024-11-29 11:50:08 +0700

    push lần 13:  viết khung các hàm thực hiện chức năng order

--- POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Order_Controller.java
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Order_Controller.java
@@ -5,7 +5,33 @@ import javafx.event.ActionEvent;
 public class Order_Controller {
   //khai báo biến
   //hàm thực hiện chương trình
+  //xóa sản phẩm khi đã thêm vào bill
   public void delete_Item(ActionEvent event) {
+
+  }
+  //khuyến mãi
+  public void Coupon(ActionEvent event) {
+
+  }
+  //hủy giao dịch
+  public void cancel_Bill(ActionEvent event) {
+
+  }
+  //button mở modal các chức năng khác
+  public void orther_Functions(ActionEvent event) {
+
+  }
+  //báo cáo nhanh
+  public void fast_Report(ActionEvent event) {
+
+  }
+  //thanh toán hóa đơn
+  public void pay_Bill(ActionEvent event) {
+
+  }
+  //tìm kiếm sản phẩm
+  public void search_Item(ActionEvent event) {
     
   }
+
 }

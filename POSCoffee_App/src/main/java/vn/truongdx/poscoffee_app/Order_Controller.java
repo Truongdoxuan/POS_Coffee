b269b617052605d445858a7a89dfbcc8296c5fb3
Xuân Trường <dxt8104@gmail.com>
2024-11-29 12:21:01 +0700

    push lần 15

--- POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Order_Controller.java
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/Order_Controller.java
@@ -1,10 +1,28 @@
 package vn.truongdx.poscoffee_app;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class Order_Controller {
   //khai báo biến
+  @FXML
+  TextField txt_dayKD, txt_time, txt_workshifts, txt_poscode, txt_version;
+  @FXML
+  TextField txt_searchItem;
+  @FXML
+  Button btn_orther, btn_topingsize;
+
   //hàm thực hiện chương trình
+  //hàm khởi chạy màn hình trước khi thực hiện chức năng
+  @FXML
+  public void initialize() {
+    //mã cửa hàng và mã version (tự đặt)
+    txt_poscode.setText("POS001");
+    txt_version.setText("V1.0.0");
+  }
   //xóa sản phẩm khi đã thêm vào bill
   public void delete_Item(ActionEvent event) {
 
@@ -46,6 +64,6 @@ public class Order_Controller {
   }
   //thay đổi kích cỡ sản phẩm, thêm toping
   public void change_SizeToping(ActionEvent event) {
-    
+
   }
 }

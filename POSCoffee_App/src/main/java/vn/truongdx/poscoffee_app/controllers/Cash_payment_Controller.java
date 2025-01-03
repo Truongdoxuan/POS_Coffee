c69737a3f76a203f3dfd80fa0a52bd2c4a7fdccf
Xuân Trường <dxt8104@gmail.com>
2025-01-03 11:36:40 +0700

    push lần 42: thiết kế modal thực hiện chức năng thanh toán bằng tiền mặt

---
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/controllers/Cash_payment_Controller.java
@@ -0,0 +1,16 @@
+package vn.truongdx.poscoffee_app.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Cash_payment_Controller {
+  //khai báo biến
+  @FXML
+  TextField txt_totalbill, txt_cashSend, txt_cashResend;
+  @FXML
+  Button btn_openKet;
+
+  //hàm thực hiện chức năng
+  
+}
--- POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/controllers/Payment_Controller.java
+++ POSCoffee_App/src/main/java/vn/truongdx/poscoffee_app/controllers/Payment_Controller.java
@@ -1,4 +1,25 @@
 package vn.truongdx.poscoffee_app.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+
 public class Payment_Controller {
+  //khai báo biến
+  @FXML
+  Button btn_cash, btn_banking, btn_momo, btn_close;
+  //hàm thực hiện chức năng
+  public void pay_Cash(ActionEvent event){
+
+  }
+  public void pay_Banking(ActionEvent event) {
+
+  }
+  public void pay_Momo(ActionEvent event) {
+
+  }
+  public void close_Modal() {
+
+  }
 }
